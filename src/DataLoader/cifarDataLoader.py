fd17317c41cdcbc323685bf67c85e202e2b14698
Mohamed Gamal <mgtmprog@gmail.com>
2021-01-03 01:30:35 +0200

    adding code skeleton for DataLoader

---
+++ src/DataLoader/cifarDataLoader.py
@@ -0,0 +1,12 @@
+from src.DataLoader.dataLoader import DataLoader
+
+
+class CifarDataLoader(DataLoader):
+    def __init__(self):
+        pass
+
+    def load(self, download=False):
+        pass
+
+    def getData(self):
+        pass
---
+++ src/DataLoader/dataLoader.py
@@ -0,0 +1,15 @@
+class DataLoader:
+    def __init__(self):
+        pass
+
+    def load(self):
+        pass
+
+    def divide(self):
+        pass
+
+    def prepare(self):
+        pass
+
+    def getData(self):
+        pass
