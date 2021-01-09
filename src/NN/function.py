0d7db3f7abd2114c8aa44c1096779a86c57bc965
Mohamed Gamal <mgtmprog@gmail.com>
2021-01-09 02:01:20 +0200

    Sigmoid class re-written with docs

---
+++ src/NN/function.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+
+
+class Function(ABC):
+    """
+    - This class is the base for other classes such as Layer, Activation Functions, etc.
+    - The class defines an interface of how the children objects be used
+
+    >>> f = Function1()             # make an object from Function1 (child of Function)
+    >>> out = f(X)                  # calculate the function output at input X
+    >>> local_grad = f.local_grad   # get the function local grad at the input X
+
+    - All subclasses must implement (_forward, _calc_local_grad) functions
+    """
+    def __init__(self):
+        """ Initializing Cache variables to store the function output and local_grad"""
+        self._cache = None                  # cache the function output
+        self._grad = {}                     # cache function local grad
+
+    def __call__(self, x, *args, **kwargs):
+        """Perform the function forward path f(x), calculate the function gradient with respect to x"""
+        self._cache = self._forward(x, *args, **kwargs)                 # forward path
+        self.__grad = self._calc_local_grad(x, *args, **kwargs)         # Gradient Calculation, caching
+        return self._cache
+
+    @abstractmethod
+    def _forward(self, x, *args, **kwargs):
+        """ This function resembles function forward path (f(x)), must be implemented """
+        pass
+
+    @abstractmethod
+    def _calc_local_grad(self, x, *args, **kwargs):
+        """ This function calculated the gradient (∇f = ∂f/∂x) of the function with respect to input x"""
+        pass
+
+    @property
+    def local_grad(self):
+        """Local grad getter"""
+        return self.__grad
+
