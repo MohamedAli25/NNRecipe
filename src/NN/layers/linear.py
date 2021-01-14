50450e802f66b586fa5920257fc25f96a1610945
Mohamed Gamal <mgtmprog@gmail.com>
2021-01-14 10:53:59 +0200

    added pydocs

--- src/NN/layers/linear.py
+++ src/NN/layers/linear.py
@@ -1,37 +1,87 @@
 from __future__ import annotations
-
 import numpy as np
-from .layer import Layer
+from src.NN.layers.layer import Layer
 from src.NN.function import Function
 
 
 class Linear(Layer):
-    def __init__(self, in_dim, out_dim, acivation: Function, batch_size=1, *args, **kwargs):
-        super(Linear, self).__init__(in_dim, out_dim)
-        self.activation = acivation
-        self._init_weights(self.in_dim, self.out_dim, batch_size)
-    
-    def _init_weights(self, in_dim, out_dim, batch_size):
-        factor = np.tanh(1/in_dim)      # 1/sqrt(in_dim)    # 1/sqrt(indim + out_dim)
-        self._weights = np.random.rand(out_dim, in_dim) * factor
-        # self._bias = np.random.rand(out_dim, batch_size) * factor
-        self._bias = np.ones((out_dim, batch_size))
+    """
+    This Class represents a Linear Layer (Dense - Fully connected)
 
-    def _forward(self, x:np.ndarray): 
-        """ Returns the output after applying the activation function"""
-        return self.activation((np.dot(self._weights, x.T) + self._bias))
+    Linear Layer is responsible for:
+        - Calculating the forward path              Z = W * X.T
+        - Calculating activation of the layer       Y = Act(Z)
+        - Calculating local gradients that will be used by the optimizers
+
+    Gradient Calculated are:
+        1. dW: ∂Y/∂Z * ∂Z/∂W = activation gradient * X
+        2. dX: ∂Y/∂Z * ∂Z/∂X = activation gradient * W
+        3. dB: ∂Y/∂Z * ∂Z/∂B = activation gradient * 1
+    """
+    def __init__(self, in_dim, out_dim, activation, batch_size=1, **kwargs):
+        """
+        Initializes the layer by calling base class constructor to create weights and bias and initialize them
+
+        :param in_dim: number of neurons of the previous layer
+        :type in_dim: int
+        :param out_dim: number of neurons of the current layer
+        :type out_dim: int
+        :param activation: activation function that will be used
+        :type activation: Function
+        :param batch_size: batch size
+        :type batch_size: int
+        :keyword weights: Initial value for layer weights
+        :keyword bias:  Initial value for layer bias
+
+        :raise TypeError: When the given initial data doesn't have the expected type
+        :raise ShapeError: When the given initial data doesn't have the expected shape
+
+        """
+        self.__activation = activation
+        self.__batch_size = batch_size
+        super(Linear, self).__init__(in_dim, out_dim, **kwargs)
+
+    
+    def _init_params(self):
+        """
+        Initializes layer parameters (weights and bias)
+
+        Many different initialize schemes could be used:        # TODO add different init_factors that can be used (mar)
+            -
+            -
+            -
+
+        """
+        factor = np.tanh(1/self._in_dim) # factor that will be used to normalize params
+        self._weights = np.random.rand(self._out_dim, self._in_dim) * factor # init weights
+        # TODO make initializing bias and weights with a pre defined values a feature
+        self._bias = np.random.rand(self._out_dim, self.__batch_size) * factor
+        # self._bias = np.ones((self._out_dim, self.__batch_size)) # init bias
+
+    def _forward(self, x):
+        """
+        Calculates forward path (W*X.t) then apply activation function
+        :param x: input to the layer (output from the previous layer)
+        :type x: np.ndarray
+        :rtype: np.ndarray
+        """
+        return self.__activation((np.dot(self._weights, x.T) + self._bias))
 
     def _calc_local_grad(self, x):
         """
-            dy/dz = self.activation.local_grad
-            d_z/d_w = x
-            d_z/d_in = self.weights        z = np.dot(w, in.T)
-            d_z/db = 1
-            res = np.dot(dy/dz, dz/d_in)
-            np.multiply(sigma, res)     not here
+        Local gradient calculations
+
+        Gradient Calculated are:
+            1. dW: ∂Y/∂Z * ∂Z/∂W = activation gradient * X
+            2. dX: ∂Y/∂Z * ∂Z/∂X = activation gradient * W
+            3. dB: ∂Y/∂Z * ∂Z/∂B = activation gradient * 1
+
+        :param x: input to the layer (output from the previous layer)
+        :type x: np.ndarray
+        :rtype: dict[str, np.ndarray]
         """
         return {
-            'dW': np.dot(self.activation.local_grad, x),
-            'dX': np.multiply(self.activation.local_grad, self.weights),
-            'dB': self.activation.local_grad
+            'dW': np.dot(self.__activation.local_grad, x),
+            'dX': np.multiply(self.__activation.local_grad, self.weights),
+            'dB': self.__activation.local_grad
         }
