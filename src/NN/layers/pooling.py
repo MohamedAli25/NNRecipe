fe2fee8bcb66497e9a20eeae5783c588b54180c4
theMeanScore <m.ad.ali2165@gmail.com>
2021-01-16 03:17:50 +0200

    Implemented Pooling Layers
    Co-authored-by: alien19 <1601374@eng.asu.edu.eg>
    Co-authored-by: jasmin285 <jasminalaa@hotmail.com>

--- src/NN/layers/pooling.py
+++ src/NN/layers/pooling.py
@@ -2,26 +2,101 @@ from __future__ import annotations
 
 import numpy as np
 from src.NN.function import Function
+from enum import Enum, auto
+
+
+class PaddingType(Enum):
+    SAME = auto()
+    VALID = auto()
+
 
 class MaxPool2D(Function):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, kernelSize, strides, padding=PaddingType.SAME, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        pass
-    def forward(self, x):
-        pass
-    def backward(self, dL):
-        pass
-    def local_grad(self):
-        pass
+        if isinstance(kernelSize, int):
+            kernelSize = (kernelSize, kernelSize)
+        if isinstance(strides, int):
+            strides = (strides, strides)
+        # General format (x,y)
+        self.kernelSize: tuple = kernelSize
+        # General format (x,y)
+        self.strides: tuple = strides
+        self.padding: PaddingType = padding
+
+    def __call__(self, x, *args, **kwargs):
+        return self._forward(x)
+
+    def _forward(self, x, *args, **kwargs):
+        # numOfBatches, numOfChannels, height, width = x.shape
+        height, width = x.shape
+        # Calculate the size of the output
+        # ouputSize = (0, 0)
+        # padding = (0, 0)
+        # newInput = None
+        if self.padding == PaddingType.VALID:
+            outputHeight = (height - self.kernelSize[0]) // self.strides[0] + 1
+            outputWidth = (width - self.kernelSize[1]) // self.strides[1] + 1
+            outputSize = (outputHeight, outputWidth)
+            newInput = x
+        else:
+            outputSize = (height, width)
+            paddingHeight = ((self.strides[0] - 1) * height - self.strides[0] + self.kernelSize[0]) // 2
+            paddingWidth = ((self.strides[1] - 1) * width - self.strides[1] + self.kernelSize[1]) // 2
+            newInput = np.zeros(outputSize)
+            newInput[paddingHeight:paddingWidth + height, paddingWidth:paddingWidth + width] = x
+        output = np.zeros(outputSize)
+        outputHeight = outputSize[0]
+        outputWidth = outputSize[1]
+        for i in range(outputHeight):
+            for j in range(outputWidth):
+                heightStart = i * self.strides[0]
+                heightEnd = heightStart + self.strides[0]
+                widthStart = j * self.strides[1]
+                widthEnd = widthStart + self.strides[1]
+                output[i][j] = np.max(newInput[heightStart:heightEnd, widthStart:widthEnd])
 
 
 class AvgPool2D(Function):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, kernelSize, strides, padding=PaddingType.SAME, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        pass
+        if isinstance(kernelSize, int):
+            kernelSize = (kernelSize, kernelSize)
+        if isinstance(strides, int):
+            strides = (strides, strides)
+        # General format (x,y)
+        self.kernelSize: tuple = kernelSize
+        # General format (x,y)
+        self.strides: tuple = strides
+        self.padding: PaddingType = padding
+
+    def __call__(self, x, *args, **kwargs):
+        return self._forward(x)
+
     def forward(self, x):
-        pass
-    def backward(self, dL):
-        pass
-    def local_grad(self):
-        pass
+        # numOfBatches, numOfChannels, height, width = x.shape
+        height, width = x.shape
+        # Calculate the size of the output
+        # ouputSize = (0, 0)
+        # padding = (0, 0)
+        # newInput = None
+        if self.padding == PaddingType.VALID:
+            outputHeight = (height - self.kernelSize[0]) // self.strides[0] + 1
+            outputWidth = (width - self.kernelSize[1]) // self.strides[1] + 1
+            outputSize = (outputHeight, outputWidth)
+            newInput = x
+        else:
+            outputSize = (height, width)
+            paddingHeight = ((self.strides[0] - 1) * height - self.strides[0] + self.kernelSize[0]) // 2
+            paddingWidth = ((self.strides[1] - 1) * width - self.strides[1] + self.kernelSize[1]) // 2
+            newInput = np.zeros(outputSize)
+            newInput[paddingHeight:paddingWidth + height, paddingWidth:paddingWidth + width] = x
+        output = np.zeros(outputSize)
+        outputHeight = outputSize[0]
+        outputWidth = outputSize[1]
+        for i in range(outputHeight):
+            for j in range(outputWidth):
+                heightStart = i * self.strides[0]
+                heightEnd = heightStart + self.strides[0]
+                widthStart = j * self.strides[1]
+                widthEnd = widthStart + self.strides[1]
+                output[i][j] = np.average(newInput[heightStart:heightEnd, widthStart:widthEnd])
