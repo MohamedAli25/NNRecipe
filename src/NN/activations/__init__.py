1497d30e418846e10635fd7785154224b4f2f3e9
Mohamed Gamal <mgtmprog@gmail.com>
2021-01-20 06:21:32 +0200

    added packaging files

---
+++ src/NN/activations/__init__.py
@@ -0,0 +1,6 @@
+from .HardTanh import HardTanh
+from .Identity import Identity
+from .LeakyRelu import LeakyReLU
+from .Relu import ReLU
+from .Sigmoid import Sigmoid
+from .Tanh import Tanh
---
+++ src/NN/losses/__init__.py
@@ -0,0 +1,1 @@
+from .MeanSquared import MeanSquaredLoss
