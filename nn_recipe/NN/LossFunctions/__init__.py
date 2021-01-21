73d8593d7756ad704ae4029be38439240c6e76fb
Mohamed Gamal <mgtmprog@gmail.com>
2021-01-21 10:29:53 +0200

    preparing for setup

--- nn_recipe/NN/LossFunctions/__init__.py
+++ nn_recipe/NN/LossFunctions/__init__.py
@@ -1,6 +1,7 @@
 from .meanSquared import MeanSquaredLoss
 from .crossEntropy import CrossEntropyLoss
+from .hingeLoss import HingeLoss
 
 __all__ = [
-    "MeanSquaredLoss", "CrossEntropyLoss"
+    "MeanSquaredLoss", "CrossEntropyLoss", "HingeLoss"
 ]
