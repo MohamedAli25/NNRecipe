1bff772669dfb91982feeeb4385bb38eeb7b1b12
Mohamed Gamal <mgtmprog@gmail.com>
2021-01-23 19:47:43 +0200

    bug fixing

--- mnist_test.py
+++ mnist_test.py
@@ -7,49 +7,51 @@ from nn_recipe.utility import OneHotEncoder
 
 import numpy as np
 
-# you can download the whole 60K example from http://yann.lecun.com/exdb/mnist/
-# i attached bellow a sample from it just a 1000 example
-# change the path bellow to mnist_1k.csv
-X = np.loadtxt("C:\\Users\\mgtmP\\Desktop\\mnist_1k.txt", delimiter=",")
-Y = X[:,0].reshape((-1, 1))
-X = X[:,1:]
-X = X / 255
-
-encoder = OneHotEncoder(
-    types=[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0],
-    active_state=1,
-    inactive_state=0
-)
-Y = encoder.encode(Y)
-
-net = Network(
-    layers=[
-        Linear(in_dim=784, out_dim=25, activation=Sigmoid()),
-        Linear(in_dim=25, out_dim=10, activation=Identity())
-    ],
-    optimizer=GD(learning_rate=0.003),
-    loss_function=MClassBipolarPerceptron(sum=True, axis=0),
-)
-net.train(X, Y, notify_func=print, batch_size=None, max_itr=1)
+# # you can download the whole 60K example from http://yann.lecun.com/exdb/mnist/
+# # i attached bellow a sample from it just a 1000 example
+# # change the path bellow to mnist_1k.csv
+# X = np.loadtxt("C:\\Users\\mgtmP\\Desktop\\mnist_1k.txt", delimiter=",")
+# Y = X[:,0].reshape((-1, 1))
+# X = X[:,1:]
+# X = X / 255
+#
+# encoder = OneHotEncoder(
+#     types=[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0],
+#     active_state=1,
+#     inactive_state=0
+# )
+# Y = encoder.encode(Y)
+#
+# net = Network(
+#     layers=[
+#         Linear(in_dim=784, out_dim=25, activation=Sigmoid()),
+#         Linear(in_dim=25, out_dim=10, activation=Identity())
+#     ],
+#     optimizer=GD(learning_rate=0.003),
+#     loss_function=MClassBipolarPerceptron(sum=True, axis=0),
+# )
+# net.train(X, Y, notify_func=print, batch_size=None, max_itr=1)
 
 
 # # TODO Bugs needed to be solved: Batch size spliting array
 # # TODO implement softmax layer
 #
 #
-# X = np.array([[5, 6, 7]])
-# loss = MClassBipolarPerceptron()
-# # print(loss(X))
-# #
-# #
-# #
-# # OneHotEncoder testing
-# Y = np.array([["Ahmed"]])
+X = np.array([[5, 6, 7],
+              [6, 7, 8]])
+loss = MClassLogisticLoss()
+# print(loss(X))
 #
-# encoder = OneHotEncoder(
-#     types=["Mohamed", "Ahmed", "Ali"],
-# )
 #
-# encoded_Y = encoder.encode(Y).T
-# loss(encoded_Y, X)
 #
+# OneHotEncoder testing
+Y = np.array([["Ahmed"],
+              ["Ali"]])
+
+encoder = OneHotEncoder(
+    types=["Mohamed", "Ahmed", "Ali"],
+)
+
+encoded_Y = encoder.encode(Y).T
+loss(encoded_Y, X)
+
--- nn_recipe/NN/LossFunctions/multiClassLogisticLoss.py
+++ nn_recipe/NN/LossFunctions/multiClassLogisticLoss.py
@@ -5,10 +5,14 @@ import numpy as np
 class MClassLogisticLoss(LossFunction):
     def _compute_loss(self, Y, Y_hat):
         expZ = np.exp(Y_hat - np.max(Y_hat)) # softmax here is a row vector for each example
+        print(expZ)
+        print(np.sum(expZ, axis=1).reshape(-1, 1))
         self.__softmax_value = expZ / np.sum(expZ, axis=1).reshape(-1, 1)
-        return -1*np.log(self.__softmax_value[0, np.argmax(Y, axis=1)])
+        print(self.__softmax_value)
+        # return -1*np.log(self.__softmax_value[0, np.argmax(Y, axis=1)])
 
     def _compute_local_grad(self, Y, Y_Hat):
-        self.__softmax_value[np.argmax(Y, axis=0), range(self.__softmax_value.shape[1])] -= 1
-        return self.__softmax_value
+        # self.__softmax_value[np.argmax(Y, axis=0), range(self.__softmax_value.shape[1])] -= 1
+        # return self.__softmax_value
+        pass
 
