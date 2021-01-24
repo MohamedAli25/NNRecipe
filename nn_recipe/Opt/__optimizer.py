6b7128fa78dd7791cc78664971ac2b896c8ef0d0
Mohamed Gamal <mgtmprog@gmail.com>
2021-01-24 12:01:41 +0200

    saving and loading functionality is added

--- nn_recipe/Opt/__optimizer.py
+++ nn_recipe/Opt/__optimizer.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 
 
 class Optimizer(ABC):
+    ID = -1
+
     @abstractmethod
     def optimize(self, y, layer, delta, opt_type: str):
         pass
@@ -16,6 +18,16 @@ class Optimizer(ABC):
             message = "Optimizer learning rate must be a scalar real number current type is " + str(learning_rate_type)
             super().__init__(message)
 
+    @abstractmethod
+    def _save(self):
+        pass
 
-def MomentumBased():
-    pass
+    def save(self):
+        out = self._save()
+        out["ID"] = self.ID
+        return out
+
+    @staticmethod
+    @abstractmethod
+    def load(data):
+        pass
